e1f3e41d8807da0505efd5492442f8f66aecfddb
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-01-05 17:11:24 +0100

    merged the atlas and section repos.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from src.gui.workflows import WorkflowProvider
-from src.gui.window import Window
 from src.gui.presenter import Presenter
-from src.repos.inmemory_atlas import InMemoryAtlasRepo
-from src.repos.inmemory_section import InMemorySectionRepo
+from src.gui.window import Window
+from src.gui.workflows import WorkflowProvider
+from src.repos.inmemory import InMemoryRepo
 from src.serializers.ome_tiff import OmeTiffSerializer
 
 win = Window(title="Registration App")
 
 presenter = Presenter(win=win)
+repo = InMemoryRepo()
+
 use_cases = WorkflowProvider(
-    section_repo=InMemorySectionRepo(),
-    atlas_repo=InMemoryAtlasRepo(),
+    section_repo=repo,
+    atlas_repo=repo,
     section_serializer=OmeTiffSerializer(),
     load_atlas_presenter=presenter,
     select_channel_presenter=presenter,
--- src/repos/inmemory.py
+++ src/repos/inmemory.py
@@ -3,12 +3,15 @@ from typing import Optional
 from src.core.atlas.load_atlas import BaseAtlasRepo
 from src.core.atlas.models import Atlas
 from src.serializers.bg_atlas import BGAtlasSerializer
+from src.core.section.base import BaseSectionRepo
+from src.core.section.models import Section
 
 
-class InMemoryAtlasRepo(BaseAtlasRepo):
+class InMemoryRepo(BaseAtlasRepo, BaseSectionRepo):
 
     def __init__(self):
         self._current_atlas: Optional[Atlas] = None
+        self._section: Optional[Section] = None
 
     def get_atlas(self, resolution_um: int) -> Atlas:
         if self._current_atlas is None or self._current_atlas.resolution_um != resolution_um:
@@ -18,3 +21,8 @@ class InMemoryAtlasRepo(BaseAtlasRepo):
     def get_current_atlas(self) -> Atlas:
         return self._current_atlas
 
+    def get_section(self) -> Optional[Section]:
+        return self._section
+
+    def save_section(self, section: Section) -> None:
+        self._section = section
