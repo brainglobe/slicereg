84c112dba1e4488c750b4293116933b466ea16cc
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-02-18 14:40:22 +0100

    t2

--- tests/models/test_section.py
+++ tests/models/test_section.py
@@ -8,12 +8,13 @@ from slicereg.models.section import Section, SliceImage, Plane
 cases = [
     ((0, 0), (0., 0., 0.)),
     ((1, 1), (1., -1., 0.)),
+    ((2, 3), (3., -2., 0.)),
 ]
 @pytest.mark.parametrize("imcoord, atlascoord", cases)
 def test_can_get_3d_position_from_2d_pixel_coordinate_in_section(imcoord, atlascoord):
     section = Section(
         image=SliceImage(
-            channels=arange(18).reshape(2, 3, 3),
+            channels=arange(24).reshape(2, 3, 4),
             pixel_resolution_um=1
         ),
         plane=Plane(x=0, y=0, theta=0),
