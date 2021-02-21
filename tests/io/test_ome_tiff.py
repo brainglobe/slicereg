3dbfa588f7067e843368653e76cd0aac2ea979f1
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-02-21 22:13:41 +0100

    bugfix: slice reader converts pixel size to resolution

--- tests/io/test_ome_tiff.py
+++ tests/io/test_ome_tiff.py
@@ -7,10 +7,10 @@ from slicereg.io.ome_tiff import OmeTiffReader
 cases = [
     ("data/RA_10X_scans/MeA/S1_09032020.ome.tiff", (2, 2816, 4198), 2.77)
 ]
-@pytest.mark.parametrize("filename,shape,res", cases)
-def test_tiff_reader_gets_sliceimages_from_example_files(filename, shape, res):
+@pytest.mark.parametrize("filename,shape,pixel_size_um", cases)
+def test_tiff_reader_gets_sliceimages_from_example_files(filename, shape, pixel_size_um):
     slice_image = OmeTiffReader().read(filename=filename)
     assert slice_image.channels.ndim == 3
     assert slice_image.channels.shape == shape
     assert not np.any(np.isnan(slice_image.channels))
-    assert slice_image.pixel_resolution_um == approx(res, abs=1e-2)
+    assert slice_image.pixel_resolution_um == approx(1 / pixel_size_um, abs=1e-2)
