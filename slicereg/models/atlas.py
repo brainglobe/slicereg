4159728c245c7f26c63f81a527fa8227d3a1a02c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-02-24 12:11:42 +0100

    added slice method to Atlas

--- slicereg/models/atlas.py
+++ slicereg/models/atlas.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 from typing import Tuple
 
+import numpy as np
 from numpy import ndarray
 from vispy.util.transforms import scale, translate
+
+from slicereg.models.image import ImageData
+from slicereg.models.section import Section
+from slicereg.models.transforms import Plane3D
 
 
 @dataclass
@@ -14,3 +19,10 @@ class Atlas:
     def affine_transform(self) -> ndarray:
         w, h, d = self.volume.shape
         return (translate((-w / 2, -h / 2, -d / 2)) @ scale((self.resolution_um,) * 3)).T
+
+    def slice(self, plane: Plane3D, thickness_um: int) -> Section:
+        return Section(
+            image=ImageData(channels=np.zeros((1, 3, 3)), pixel_resolution_um=self.resolution_um),
+            plane_3d=plane,
+            thickness_um=thickness_um
+        )
--- tests/models/test_atlas.py
+++ tests/models/test_atlas.py
@@ -3,6 +3,8 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 from slicereg.models.atlas import Atlas
+from slicereg.models.section import Section
+from slicereg.models.transforms import Plane3D, Plane2D
 
 
 @given(res=integers(1, 1000), w=integers(1, 100), h=integers(1, 100), d=integers(1, 100))
@@ -16,3 +18,17 @@ def test_atlas_matrix_is_scaled_to_um_according_to_resolution_and_shape(res, w, 
     ]
     observed = atlas.affine_transform
     assert np.all(np.isclose(observed, np.array(expected)))
+
+
+def test_slicing_an_atlas_gets_a_new_section_with_correct_parameters():
+    atlas = Atlas(
+        volume=np.broadcast_to(np.array([10, 20, 30]), (3, 3, 3)).swapaxes(0, 2),
+        resolution_um=1
+    )
+    plane = Plane3D(x=0)
+    section = atlas.slice(plane, thickness_um=16.)
+    assert isinstance(section, Section)
+    assert section.plane_3d == plane
+    assert section.plane_2d == Plane2D()
+    assert section.image.pixel_resolution_um == atlas.resolution_um
+    assert section.image.channels.shape == (1, 3, 3)
