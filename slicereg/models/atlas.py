9d5a12f23b6e10279c56732dfea9ab44b09b028e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-03-01 12:59:54 +0100

    simplified slice to single-axis for early iteration performance

--- slicereg/models/atlas.py
+++ slicereg/models/atlas.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from numpy import ndarray, newaxis
+from numpy import ndarray, newaxis, floor, zeros
 from scipy.ndimage import affine_transform
 from vispy.util.transforms import scale, translate
 
@@ -20,10 +20,34 @@ class Atlas:
         return (translate((-w / 2, -h / 2, -d / 2)) @ scale((self.resolution_um,) * 3)).T
 
     def slice(self, plane: Plane3D) -> Section:
-        new_volume = affine_transform(self.volume, matrix=plane.affine_transform, cval=0.)
-        slice_image = new_volume[:, :, 0][newaxis, :, :]
+        if plane.rotation == (0, -90, 0):
+            if 0 <= plane.x < self.volume.shape[0]:
+                slice_image = self.volume[int(floor(plane.x)), :, :][newaxis, :, :]
+            else:
+                h, w = self.volume.shape[:2]
+                slice_image = zeros((1, h, w))
+
+        elif plane.rotation == (90, 0, 0):
+            if 0 <= plane.y < self.volume.shape[1]:
+                slice_image = self.volume[:, int(floor(plane.y)), :][newaxis, :, :]
+            else:
+                h, w = self.volume.shape[:2]
+                slice_image = zeros((1, h, w))
+        elif plane.rotation == (0, 0, 0):
+            if 0 <= plane.z < self.volume.shape[2]:
+                slice_image = self.volume[:, :, int(floor(plane.z))][newaxis, :, :]
+            else:
+                h, w = self.volume.shape[:2]
+                slice_image = zeros((1, h, w))
+        else:
+            # new_volume = affine_transform(self.volume, matrix=plane.affine_transform, cval=0.)
+            # slice_image = new_volume[:, :, 0][newaxis, :, :]
+            raise NotImplementedError(f"No atlas rotation slicing implemented yet., {plane.rotation}")
+
+
         return Section(
             image=ImageData(channels=slice_image, pixel_resolution_um=self.resolution_um),
             plane_3d=plane,
             thickness_um=self.resolution_um
         )
+
