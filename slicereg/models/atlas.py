3ed11cdf0a82fa94e7fb382512c71b980ecba676
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-03-09 22:01:18 +0100

    re-added original slice method

--- slicereg/models/atlas.py
+++ slicereg/models/atlas.py
@@ -20,30 +20,8 @@ class Atlas:
         return (translate((-w / 2, -h / 2, -d / 2)) @ scale((self.resolution_um,) * 3)).T
 
     def slice(self, plane: Plane3D) -> Section:
-        if plane.rotation == (0, -90, 0):
-            if 0 <= plane.x < self.volume.shape[0]:
-                slice_image = self.volume[int(floor(plane.x)), :, :][newaxis, :, :]
-            else:
-                h, w = self.volume.shape[:2]
-                slice_image = zeros((1, h, w))
-
-        elif plane.rotation == (90, 0, 0):
-            if 0 <= plane.y < self.volume.shape[1]:
-                slice_image = self.volume[:, int(floor(plane.y)), :][newaxis, :, :]
-            else:
-                h, w = self.volume.shape[:2]
-                slice_image = zeros((1, h, w))
-        elif plane.rotation == (0, 0, 0):
-            if 0 <= plane.z < self.volume.shape[2]:
-                slice_image = self.volume[:, :, int(floor(plane.z))][newaxis, :, :]
-            else:
-                h, w = self.volume.shape[:2]
-                slice_image = zeros((1, h, w))
-        else:
-            # new_volume = affine_transform(self.volume, matrix=plane.affine_transform, cval=0.)
-            # slice_image = new_volume[:, :, 0][newaxis, :, :]
-            raise NotImplementedError(f"No atlas rotation slicing implemented yet., {plane.rotation}")
-
+        new_volume = affine_transform(self.volume, matrix=plane.affine_transform, cval=0.)
+        slice_image = new_volume[:, :, 0][newaxis, :, :]
 
         return Section(
             image=ImageData(channels=slice_image, pixel_resolution_um=self.resolution_um),
