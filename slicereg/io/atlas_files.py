d4dc83404a6b4b1e99038e078a8d4b358d8175cf
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-07 15:24:02 +0200

    added tiffile atlas loading function
    Co-authored-by: Emily Jane Dennis <emilyjanedennis@gmail.com>

---
+++ slicereg/io/atlas_files.py
@@ -0,0 +1,6 @@
+from slicereg.models.atlas import Atlas
+import tifffile as tif
+
+
+def load_atlas_from_files(tiff_stack_filename: str, resolution: float) -> Atlas:
+    return Atlas(volume=tif.imread(tiff_stack_filename), resolution_um=resolution)
