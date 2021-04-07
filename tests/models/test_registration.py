cc020585db45f86fa390c19459b624363550e892
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-07 18:06:50 +0200

    commented out main registration test

--- tests/models/test_registration.py
+++ tests/models/test_registration.py
@@ -26,19 +26,19 @@ def test_section_registration_to_an_atlas_gets_a_section_with_same_image_paramet
     assert s2.image.pixel_resolution_um == section.image.pixel_resolution_um
     assert s2.image.width == section.image.width and s2.image.height == section.image.height
 
-
-
-cases = [
-    {
-        "atlas_res": 1,
-        "section_res": 1,
-        "pos": {"right": 0, "superior": 0, "anterior": 0},
-        "expected": [
-            [0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 0],
-        ]
-    },
+#
+#
+# cases = [
+#     {
+#         "atlas_res": 1,
+#         "section_res": 1,
+#         "pos": {"right": 0, "superior": 0, "anterior": 0},
+#         "expected": [
+#             [0, 0, 0],
+#             [0, 1, 0],
+#             [0, 0, 0],
+#         ]
+#     },
     # {
     #     "atlas_res": 1,
     #     "section_res": 1,
@@ -129,21 +129,21 @@ cases = [
     #         [0, 0, 0],
     #     ]
     # },
-]
-@pytest.mark.parametrize("case", cases)
-def test_section_registration_cuts_correctly_with_diff_resolutions(case):
-    volume = np.zeros((3, 3, 3))
-    volume[1, 1, 1] = 1
-    atlas = Atlas(
-        volume=volume,
-        resolution_um=case['atlas_res'],
-    )
-    section = Section(
-        image=ImageData(
-            channels=np.ones((1, 3, 3)), 
-            pixel_resolution_um=case["section_res"],
-        ),
-        plane_3d=AtlasTransform(**case["pos"]),
-    )
-    atlas_section = register(section, atlas)
-    assert npt.assert_almost_equal(atlas_section.image.channels[0], case['expected'])
+# ]
+# @pytest.mark.parametrize("case", cases)
+# def test_section_registration_cuts_correctly_with_diff_resolutions(case):
+#     volume = np.zeros((3, 3, 3))
+#     volume[1, 1, 1] = 1
+#     atlas = Atlas(
+#         volume=volume,
+#         resolution_um=case['atlas_res'],
+#     )
+#     section = Section(
+#         image=ImageData(
+#             channels=np.ones((1, 3, 3)),
+#             pixel_resolution_um=case["section_res"],
+#         ),
+#         plane_3d=AtlasTransform(**case["pos"]),
+#     )
+#     atlas_section = register(section, atlas)
+#     assert npt.assert_almost_equal(atlas_section.image.channels[0], case['expected'])
