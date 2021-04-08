d2cbc7b241d94c67e27e2ba6a93a7a3013a5c04a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-08 15:25:36 +0200

    ij to xyz with theta rotation

--- tests/models/test_section.py
+++ tests/models/test_section.py
@@ -14,15 +14,12 @@ from slicereg.models.transforms import AtlasTransform
 
 real_floats = partial(floats, allow_nan=False, allow_infinity=False)
 
-np.set_printoptions(precision=5, suppress=True)
-
-
 @given(
-    i=integers(0, 2), j=integers(0, 3),
+    i=integers(0, 1e5), j=integers(0, 1e5),
     i_shift=real_floats(-2, 2), j_shift=real_floats(-2, 2),
     theta=real_floats(-500, 500),
-    x=real_floats(), y=real_floats(), z=real_floats(),
-    res=real_floats(.001, 1000)
+    x=real_floats(-1e5, 1e5), y=real_floats(-1e5, 1e5), z=real_floats(-1e5, 1e5),
+    res=real_floats(1e-4, 1000)
 )
 def test_can_get_3d_position_from_2d_pixel_coordinate_in_section(i, j, i_shift, j_shift, theta, x, y, z, res):
     section = Section(
@@ -30,15 +27,23 @@ def test_can_get_3d_position_from_2d_pixel_coordinate_in_section(i, j, i_shift, 
         pixel_resolution_um=res,
         plane_3d=AtlasTransform(x=x, y=y, z=z),
     )
-    t = radians(theta)
+    t = -radians(theta)  # image is left-handed, so flip rotation
     xyz = section.map_ij_to_xyz(i=i, j=j)  # observed 3D positions
+
+    # do shift first, to make final 2d rotation calculation easier https://academo.org/demos/rotation-about-point/
+    j2 = ( j + (j_shift * section.image.width))
+    i2 = (-i - (i_shift * section.image.height))
+
     expected = np.array([
-        [( j + (j_shift * section.image.width))  * cos(t) * res + x],
-        [(-i - (i_shift * section.image.height)) * sin(t) * res + y],
+        [(j2 * cos(t) + i2 * sin(t)) * res + x],
+        [(i2 * cos(t) - j2 * sin(t)) * res + y],
         [z],
         [1],
     ])
-    npt.assert_almost_equal(xyz, expected)
+    npt.assert_almost_equal(xyz, expected, decimal=4)
+
+
+np.set_printoptions(precision=5, suppress=True)
 
 #
 # @given(width=integers(1, 1000), height=integers(1, 1000), channels=integers(1, 6),
