0c9ad139613e008cf19e6733e8851b1eaf46a06b
philshams <32060463+philshams@users.noreply.github.com>
2021-04-09 12:25:04 +0100

    Add labels to sliders
    Co-authored-by: Nicholas A. Del Grosso <delgrosso.nick@gmail.com>

--- slicereg/gui/slider.py
+++ slicereg/gui/slider.py
@@ -10,15 +10,18 @@ class LabelledSliderWidget:
         self.slider = QSlider(Qt.Horizontal)
         self.layout.addWidget(self.slider)
 
-        self.label = QLabel(text=label)
-        self.layout.addWidget(self.label)
+        self.value_label = QLabel(text='0')
+        self.layout.addWidget(self.value_label)
+
+        self.name_label = QLabel(text=label)
+        self.layout.addWidget(self.name_label)
 
         self.slider.setMinimum(int(min))
         self.slider.setMaximum(int(max))
         self.slider.valueChanged.connect(self._on_slider_valuechange)
 
     def _on_slider_valuechange(self, value: int):
-        self.label.setText(str(value))
+        self.value_label.setText(str(value))
 
     @property
     def connect(self):
