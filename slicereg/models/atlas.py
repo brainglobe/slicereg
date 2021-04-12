5f945d1a0d60c026d65f8ba7f5ea86d4ffba9548
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-12 02:09:13 +0200

    removed unused matrix

--- slicereg/models/atlas.py
+++ slicereg/models/atlas.py
@@ -12,20 +12,12 @@ class Atlas:
     def affine_transform(self) -> np.ndarray:
         # need -90 degree rotation (ignore left-handed nature of Z coords, since don't exist)
         ijk_to_xyz_matrix = np.array([
-            [0, 1, 0, 0],
+            [ 0, 1, 0, 0],
             [-1, 0, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
+            [ 0, 0, 1, 0],
+            [ 0, 0, 0, 1],
         ])
-
-        w, h, d = self.volume.shape
-        translate_matrix = np.array([
-            [1, 0, 0, -w / 2],
-            [0, 1, 0, -h / 2],
-            [0, 0, 1, -d / 2],
-            [0, 0, 0, 1],
-        ])
-        return self.scale_matrix @ ijk_to_xyz_matrix # @ translate_matrix
+        return self.scale_matrix @ ijk_to_xyz_matrix
 
     @property
     def scale_matrix(self) -> np.ndarray:
