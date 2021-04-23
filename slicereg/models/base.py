8de276130564472d6d53e3bc7619fe94d1034385
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-23 15:33:07 +0200

    added convenience utility for dealing with nested immutables

--- slicereg/models/base.py
+++ slicereg/models/base.py
@@ -5,5 +5,25 @@ from dataclasses import is_dataclass, replace
 class FrozenUpdater(ABC):
 
     def update(self, **kwargs):
+        """
+        Returns a new object with updated attributes.  If '__' is used, can update a nested attribute.
+
+        Examples:
+            >>> obj = Object(x=0, y=0, z=0)
+            >>> obj.update(x=3, y=5)
+            Object(x=3, y=5, z=0)
+
+            >>> obj = Object(x=0, y=Object(x=0, y=2))
+            >>> obj.update(x=2, y__x=5)
+            Object(x=2, y=Object(x=5, y=2))
+        """
         assert is_dataclass(self), "only works on dataclasses."
-        return replace(self, **kwargs)
+
+        top_attrs = {k: v for k, v in kwargs.items() if not '__' in k}
+        deep_attrs = {k: v for k, v in kwargs.items() if '__' in k}
+        for key, value in deep_attrs.items():
+            attr, nextattr = key.split('__')
+            val = replace(getattr(self, attr), **{nextattr: value})
+            top_attrs[attr] = val
+
+        return replace(self, **top_attrs)
--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from slicereg.models.base import FrozenUpdater
 
 
-def test_setter_mixin_updates_dataframes():
+def test_updater_mixin_updates_dataframes():
     @dataclass(frozen=True)
     class A(FrozenUpdater):
         a: int
@@ -13,3 +13,34 @@ def test_setter_mixin_updates_dataframes():
     a = A(3, 4, 5)
     a2 = a.update(a=33)
     assert a2.a == 33
+
+
+def test_updater_mixin_updates_dataframes():
+    @dataclass(frozen=True)
+    class A(FrozenUpdater):
+        a: int
+        b: int
+        c: float
+
+    a = A(3, 4, 5)
+    a2 = a.update(a=33, c=2)
+    assert a2.a == 33 and a2.c == 2
+
+
+def test_updater_works_on_nested_dataframes_one_level_down():
+
+    @dataclass(frozen=True)
+    class A(FrozenUpdater):
+        a: int
+
+
+    @dataclass(frozen=True)
+    class B(FrozenUpdater):
+        a: int
+        b: A
+
+    b = B(a=3, b=A(10))
+    assert b.b.a == 10
+
+    b2 = b.update(b__a=100)
+    assert b2.b.a == 100
