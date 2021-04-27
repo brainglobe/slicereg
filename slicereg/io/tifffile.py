a85a8faafca081d441a8778cd0a7abafb2dac5d3
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-27 14:27:53 +0200

    Added reader for general tiff images and tiff test data from cortexlab
    Co-authored-by: Philip Shamash <philshams@ucla.edu>

--- slicereg/io/tifffile.py
+++ slicereg/io/tifffile.py
@@ -29,3 +29,16 @@ class OmeTiffImageReader:
             "Pixels are not square"
 
         return Image(channels=image, resolution_um=float(res_x))
+
+
+class TiffImageReader:
+
+    def read(self, filename: str, resolution_um: float) -> Image:
+        f = tifffile.TiffFile(filename)
+        image = f.asarray()
+        image = image.swapaxes(0,2)
+        image = image.swapaxes(1,2)
+        assert image.ndim == 3
+        assert image.dtype == uint16
+
+        return Image(channels=image, resolution_um=resolution_um)
