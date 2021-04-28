41e04287eb4f4033e1481629f793bfcf5e3dcdfa
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-29 00:50:19 +0200

    end-to-end for loading bgatlases

--- tests/commands/load_atlas/load_atlas.feature
+++ tests/commands/load_atlas/load_atlas.feature
@@ -3,8 +3,7 @@ Feature: Allen Mouse Brain Atlas
   Load the Allen Mouse Brain Atlas for 3D registration
 
   Scenario: Load Atlas
-    Given the 25um atlas is already on my computer
-    When I ask for a 25um atlas
+    When I load the 25um allen mouse atlas
     Then a 3D volume of the 25um allen reference atlas appears onscreen.
     And it is set as the current atlas for the session.
 
--- tests/commands/load_atlas/test_load_brainglobe_atlas.py
+++ tests/commands/load_atlas/test_load_brainglobe_atlas.py
@@ -2,22 +2,45 @@ from unittest.mock import Mock
 
 import pytest
 from numpy import random
+import numpy.testing as npt
 from pytest_bdd import scenario, given, when, then
 
-from slicereg.commands.load_atlas import LoadBrainglobeAtlasCommand
-from slicereg.commands.utils import Signal
+from slicereg.gui.commands import CommandProvider
+from slicereg.gui.model import AppModel
+from slicereg.gui.views.sidebar import SidebarViewModel
+from slicereg.gui.views.volume import VolumeViewModel
 from slicereg.io.bg_atlasapi import BrainglobeAtlasReader
 from slicereg.models.atlas import Atlas
 from slicereg.repos.atlas_repo import AtlasRepo
+from slicereg.repos.section_repo import InMemorySectionRepo
+
+@pytest.fixture
+def atlas_volume():
+    return random.normal(size=(4, 4, 4))
+
+@pytest.fixture
+def model():
+    return AppModel()
+
+@pytest.fixture
+def commands(model, atlas_volume):
+    commands = CommandProvider.from_repos(atlas_repo=AtlasRepo(), section_repo=InMemorySectionRepo())
+    reader = Mock(BrainglobeAtlasReader)
+    reader.list_available.return_value = ['allen_mouse_25um']
+    reader.read.return_value = Atlas(volume=atlas_volume, resolution_um=25)
+    commands.load_atlas._reader = reader
+    commands.load_atlas.atlas_updated.connect(model.on_atlas_update)
+    return commands
 
 
 @pytest.fixture
-def command():
-    repo = Mock(AtlasRepo)
-    reader = Mock(BrainglobeAtlasReader)
-    reader.list_available.return_value = ['allen_mouse_25um']
-    reader.read.return_value = Atlas(volume=random.normal(size=(4, 4, 4)), resolution_um=25)
-    return LoadBrainglobeAtlasCommand(_repo=repo, _reader=reader, atlas_updated=Mock(Signal))
+def sidebar_view(model, commands):
+    return SidebarViewModel(_model=model, _commands=commands)
+
+
+@pytest.fixture
+def volume_view(model, commands):
+    return VolumeViewModel(_model=model, _commands=commands)
 
 
 @scenario("load_atlas.feature", "Load Atlas")
@@ -25,25 +48,17 @@ def test_outlined():
     ...
 
 
-@given("the 25um atlas is already on my computer")
-def check_atlas_exists(command):
-    # Already set.
-    pass
-
-
-@when("I ask for a 25um atlas")
-def load_atlas(command):
-    command(bgatlas_name="allen_mouse_25um")
+@when("I load the 25um allen mouse atlas")
+def load_atlas(sidebar_view):
+    sidebar_view.change_bgatlas_selection_dropdown(text="allen_mouse_25um")
+    sidebar_view.click_load_bgatlas_button()
 
 
 @then("a 3D volume of the 25um allen reference atlas appears onscreen.")
-def check_3d_atlas_data_shown(command):
-    output = command.atlas_updated.emit.call_args[1]
-    assert output['volume'].ndim == 3
-    assert output['transform'].shape == (4, 4)
-    command._reader.read.assert_called_with(path="allen_mouse_25um")
+def check_3d_atlas_data_shown(volume_view, atlas_volume):
+    npt.assert_almost_equal(volume_view.atlas_volume, atlas_volume)
 
 
 @then("it is set as the current atlas for the session.")
-def check_atlas_set_in_repo(command):
-    assert command._repo.set_atlas.call_count == 1
+def check_atlas_set_in_repo(sidebar_view):
+    assert sidebar_view.selected_bgatlas == "allen_mouse_25um"
