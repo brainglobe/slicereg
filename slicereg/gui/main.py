974e628c2f371fd96689a3661c1cd8585636fd5f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-04-28 11:34:45 +0200

    added another mac platform note

--- slicereg/gui/main.py
+++ slicereg/gui/main.py
@@ -20,8 +20,8 @@ np.set_printoptions(suppress=True, precision=2)
 def is_mac_big_sur() -> bool:
     """
     platform.system(): 'Darwin'
-    platform.release(): '20.3.0'
-    platform.mac_ver(): ('10.16', ('', '', ''), 'arm64') or ('10.16', ('', '', ''), 'x86_64')
+    platform.release(): '20.3.0' or '20.4.0'
+    platform.mac_ver(): ('10.16', ('', '', ''), 'arm64') or ('10.16', ('', '', ''), 'x86_64') or ('11.3', ('', '', ''), 'arm64')
     """
     return platform.system() == 'Darwin' and version.parse(platform.mac_ver()[0]) >= version.parse('10.16')
 
