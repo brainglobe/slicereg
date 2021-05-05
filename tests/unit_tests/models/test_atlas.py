eab4f163b02ab09c1a651ba0a6ebead7e9d9348e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-05 17:55:49 +0200

    added failing test for annotation volume

--- tests/unit_tests/models/test_atlas.py
+++ tests/unit_tests/models/test_atlas.py
@@ -22,3 +22,8 @@ def test_scale_matrix_is_scaled_to_um_according_to_resolution_and_not_shape(res,
 def test_can_get_atlas_center_in_shared_space(res, w, h, d):
     atlas = Atlas(volume=np.empty((w, h, d)), resolution_um=res)
     assert atlas.center == approx((h * res / 2, -w * res / 2, d * res / 2))
+
+
+def test_atlas_contains_an_annotation_volume():
+    atlas = Atlas(volume=np.empty((5, 4, 6)), annotation_volume=np.empty((5, 4, 6)), resolution_um=10)
+    assert atlas.annotation_volume.shape == atlas.volume.shape
