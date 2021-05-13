8dd25609baecbd5952a50752993d02e1c9d9e1a6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-13 03:52:22 +0200

    lambdas so qt compiles

--- slicereg/gui/sidebar/view.py
+++ slicereg/gui/sidebar/view.py
@@ -26,25 +26,25 @@ class SidebarView(BaseQtWidget):
 
         self.resolution_textbox = QLineEdit()
         load_atlas_layout.addWidget(self.resolution_textbox)
-        # self.resolution_textbox.textEdited.connect(self._model.update_resolution_textbox)
+        self.resolution_textbox.textEdited.connect(lambda text: self._model.update_resolution_textbox(text))
 
         self.load_atlas_from_file_button = QPushButton("Load Atlas File")
         load_atlas_layout.addWidget(self.load_atlas_from_file_button)
-        # self.load_atlas_from_file_button.clicked.connect(self.show_load_atlas_dialog)
+        self.load_atlas_from_file_button.clicked.connect(lambda: self.show_load_atlas_dialog())
 
         layout.addLayout(load_atlas_layout)
 
         self.update_bgatlas_button = QPushButton("Update Brainglobe Atlases")
         layout.addWidget(self.update_bgatlas_button)
-        # self.update_bgatlas_button.clicked.connect(self._model.click_update_bgatlas_list_button)
+        self.update_bgatlas_button.clicked.connect(lambda: self._model.click_update_bgatlas_list_button())
 
         self.list_atlas_dropdown = QComboBox()
         layout.addWidget(self.list_atlas_dropdown)
-        # self.list_atlas_dropdown.currentTextChanged.connect(self._model.change_bgatlas_selection_dropdown)
+        self.list_atlas_dropdown.currentTextChanged.connect(lambda text: self._model.change_bgatlas_selection_dropdown(text))
 
         self.load_atlas_button = QPushButton("Load Atlas")
         layout.addWidget(self.load_atlas_button)
-        # self.load_atlas_button.clicked.connect(self._model.click_load_bgatlas_button)
+        self.load_atlas_button.clicked.connect(lambda: self._model.click_load_bgatlas_button())
 
         # Load atlas controls
         load_section_layout = QHBoxLayout()
@@ -52,89 +52,89 @@ class SidebarView(BaseQtWidget):
 
         self.section_resolution_textbox = QLineEdit()
         load_section_layout.addWidget(self.section_resolution_textbox)
-        # self.section_resolution_textbox.textEdited.connect(self._model.update_section_resolution_textbox)
+        self.section_resolution_textbox.textEdited.connect(lambda text: self._model.update_section_resolution_textbox(text))
 
         # Load Section Buttons
         self.load_image_button = QPushButton("Load Section")
         load_section_layout.addWidget(self.load_image_button)
-        # self.load_image_button.clicked.connect(self.show_load_image_dialog)
+        self.load_image_button.clicked.connect(lambda: self.show_load_image_dialog())
 
         layout.addLayout(load_section_layout)
 
         self.quick_load_section_button = QPushButton("Quick Load Section")
         layout.addWidget(self.quick_load_section_button)
-        # self.quick_load_section_button.clicked.connect(self._model.click_quick_load_section_button)
+        self.quick_load_section_button.clicked.connect(lambda: self._model.click_quick_load_section_button())
 
         # Scale Sliders (Set Section Resolution)
         self.resample_widget = LabelledSliderWidget(min=15, max=200, label="Resample")
         layout.addLayout(self.resample_widget.layout)
-        # self.resample_widget.connect(self._model.slide_resample_slider)
+        self.resample_widget.connect(lambda val: self._model.slide_resample_slider(val))
 
         self.resolution_widget = LabelledSliderWidget(min=1, max=100, label="Resolution")
         layout.addLayout(self.resolution_widget.layout)
-        # self.resolution_widget.connect(self._model.slide_resolution_slider)
+        self.resolution_widget.connect(lambda val: self._model.slide_resolution_slider(val))
 
         # Movement Sliders
         self.x_slider = LabelledSliderWidget(min=-10000, max=10000, label='x')
         layout.addLayout(self.x_slider.layout)
-        # self.x_slider.connect(self._model.change_x_slider)
+        self.x_slider.connect(lambda val: self._model.change_x_slider(val))
 
         self.y_slider = LabelledSliderWidget(min=-10000, max=10000, label='y')
         layout.addLayout(self.y_slider.layout)
-        # self.y_slider.connect(self._model.change_y_slider)
+        self.y_slider.connect(lambda val: self._model.change_y_slider(val))
 
         self.z_slider = LabelledSliderWidget(min=-10000, max=10000, label='z')
         layout.addLayout(self.z_slider.layout)
-        # self.z_slider.connect(self._model.change_z_slider)
+        self.z_slider.connect(lambda val: self._model.change_z_slider(val))
 
         self.rotx_slider = LabelledSliderWidget(min=-180, max=180, label='rotx')
         layout.addLayout(self.rotx_slider.layout)
-        # self.rotx_slider.connect(self._model.change_rotx_slider)
+        self.rotx_slider.connect(lambda val: self._model.change_rotx_slider(val))
 
         self.roty_slider = LabelledSliderWidget(min=-180, max=180, label='roty')
         layout.addLayout(self.roty_slider.layout)
-        # self.roty_slider.connect(self._model.change_roty_slider)
+        self.roty_slider.connect(lambda val: self._model.change_roty_slider(val))
 
         self.rotz_slider = LabelledSliderWidget(min=-180, max=180, label='rotz')
         layout.addLayout(self.rotz_slider.layout)
-        # self.rotz_slider.connect(self._model.change_rotz_slider)
+        self.rotz_slider.connect(lambda val: self._model.change_rotz_slider(val))
 
         # Quick-rotation buttons
         buttons_layout = QHBoxLayout()
 
         self.coronal_button = QPushButton("Coronal")
         buttons_layout.addWidget(self.coronal_button)
-        # self.coronal_button.clicked.connect(self._model.click_coronal_button)
+        self.coronal_button.clicked.connect(lambda: self._model.click_coronal_button())
 
         self.annotation_atlas_button = QPushButton("Sagittal")
         buttons_layout.addWidget(self.annotation_atlas_button)
-        # self.annotation_atlas_button.clicked.connect(self._model.click_sagittal_button)
+        self.annotation_atlas_button.clicked.connect(lambda: self._model.click_sagittal_button())
 
         self.axial_button = QPushButton("Axial")
         buttons_layout.addWidget(self.axial_button)
-        # self.axial_button.clicked.connect(self._model.click_axial_button)
+        self.axial_button.clicked.connect(lambda: self._model.click_axial_button())
 
         layout.addLayout(buttons_layout)
 
         # clim sliders
         self.slice_clim_slider = QHRangeSlider(initial_values=(0., 1.), data_range=(0., 1.), step_size=0.01)
         layout.addWidget(self.slice_clim_slider)
-        # self.slice_clim_slider.valuesChanged.connect(self._model.move_clim_slice_slider)
+        self.slice_clim_slider.valuesChanged.connect(lambda vals: self._model.move_clim_slice_slider(vals))
 
         self.volume_slice_clim_slider = QHRangeSlider(initial_values=(0., 1.), data_range=(0., 1.), step_size=0.01)
         layout.addWidget(self.volume_slice_clim_slider)
-        # self.volume_slice_clim_slider.valuesChanged.connect(self._model.move_clim_volume_slider)
+        self.volume_slice_clim_slider.valuesChanged.connect(lambda vals: self._model.move_clim_volume_slider(vals))
 
         # Atlas Type Buttons
         buttons_layout = QHBoxLayout()
 
         self.registration_atlas_button = QPushButton("Registration")
         buttons_layout.addWidget(self.registration_atlas_button)
-        # self.registration_atlas_button.clicked.connect(self._model.click_registration_atlas_selector_button)
+        self.registration_atlas_button.clicked.connect(lambda: self._model.click_registration_atlas_selector_button())
 
         self.annotation_atlas_button = QPushButton("Annotation")
         buttons_layout.addWidget(self.annotation_atlas_button)
-        # self.annotation_atlas_button.clicked.connect(self._model.click_annotation_atlas_selector_button)
+        self.annotation_atlas_button.clicked.connect(lambda: self._model.click_annotation_atlas_selector_button())
 
         layout.addLayout(buttons_layout)
 
@@ -173,4 +173,4 @@ class SidebarView(BaseQtWidget):
 
     def _render_bgatlas_list_dropdown(self):
         self.list_atlas_dropdown.clear()
-        self.list_atlas_dropdown.addItems(self.bgatlas_names)
+        self.list_atlas_dropdown.addItems(self._model.bgatlas_names)
