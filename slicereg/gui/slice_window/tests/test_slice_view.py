607f0435f4466bca1ad7810789eb499300e3f14e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-14 22:17:35 +0200

    test view mouse events

--- slicereg/gui/slice_window/tests/test_slice_view.py
+++ slicereg/gui/slice_window/tests/test_slice_view.py
@@ -2,6 +2,9 @@ import inspect
 from typing import Callable, List
 from unittest.mock import Mock
 
+from vispy.scene.events import SceneMouseEvent
+from vispy.util.event import Event
+
 from slicereg.app.app_model import AppModel
 from slicereg.gui.slice_window import SliceView, SliceViewModel
 
@@ -23,3 +26,36 @@ def test_slice_view_updates_without_error_for_all_viewmodel_fields(qtbot):
         qtbot.addWidget(view.qt_widget)
         setattr(model, attr, getattr(model, attr))  # set attribute with its own value
 
+
+def test_slice_view_triggers_mouse_wheel_viewmodel_mouse_wheel(qtbot):
+    model = Mock(SliceViewModel)
+    view = SliceView(_model=model)
+    qtbot.addWidget(view.qt_widget)
+
+    event = Mock(SceneMouseEvent, delta=(1, 5))
+    view.mouse_wheel(event)
+    model.on_mousewheel_move.assert_called_with(increment=5)
+
+
+def test_slice_view_triggers_left_mouse_drag_on_viewmodel(qtbot):
+    model = Mock(SliceViewModel)
+    view = SliceView(_model=model)
+    qtbot.addWidget(view.qt_widget)
+
+    event = Mock(SceneMouseEvent, pos=(5, 10), button=1)
+    event.last_event.pos = (1, 2)
+    view.mouse_move(event)
+    model.on_left_mouse_drag.assert_called_with(x1=1, y1=2, x2=5, y2=10)
+    model.on_right_mouse_drag.assert_not_called()
+
+
+def test_slice_view_triggers_right_mouse_drag_on_viewmodel(qtbot):
+    model = Mock(SliceViewModel)
+    view = SliceView(_model=model)
+    qtbot.addWidget(view.qt_widget)
+
+    event = Mock(SceneMouseEvent, pos=(5, 10), button=2)
+    event.last_event.pos = (1, 2)
+    view.mouse_move(event)
+    model.on_left_mouse_drag.assert_not_called()
+    model.on_right_mouse_drag.assert_called_with(x1=1, y1=2, x2=5, y2=10)
--- slicereg/gui/slice_window/view.py
+++ slicereg/gui/slice_window/view.py
@@ -36,31 +36,29 @@ class SliceView(BaseQtWidget):
         self._slice.attach(ColorFilter((0., .5, 1., 1.)))
         self._slice.set_gl_state('additive', depth_test=False)
 
-        def _vispy_mouse_event(event: SceneMouseEvent) -> None:
-            if event.type == 'mouse_press':
-                event.handled = True
-
-            elif event.type == 'mouse_move':
-                if event.press_event is None:
-                    return
-                x1, y1 = event.last_event.pos
-                x2, y2 = event.pos
-                if event.button == 1:  # Left Mouse Button
-                    self._model.on_left_mouse_drag(x1=x1, x2=x2, y1=y1, y2=y2)
-                elif event.button == 2:  # Right Mouse Button
-                    self._model.on_right_mouse_drag(x1=x1, y1=y1, x2=x2, y2=y2)
-
-            elif event.type == 'mouse_wheel':
-                self._model.on_mousewheel_move(increment=int(event.delta[1]))
-
-        self._canvas.events.mouse_press.connect(_vispy_mouse_event)
-        self._canvas.events.mouse_move.connect(_vispy_mouse_event)
-        self._canvas.events.mouse_release.connect(_vispy_mouse_event)
-        self._canvas.events.mouse_wheel.connect(_vispy_mouse_event)
+        self._canvas.events.mouse_press.connect(self.mouse_press)
+        self._canvas.events.mouse_move.connect(self.mouse_move)
+        self._canvas.events.mouse_wheel.connect(self.mouse_wheel)
 
     @property
     def qt_widget(self) -> QWidget:
         return self._canvas.native
+
+    def mouse_press(self, event: SceneMouseEvent) -> None:
+        event.handled = True
+
+    def mouse_move(self, event: SceneMouseEvent) -> None:
+        if event.press_event is None:
+            return
+        x1, y1 = event.last_event.pos
+        x2, y2 = event.pos
+        if event.button == 1:  # Left Mouse Button
+            self._model.on_left_mouse_drag(x1=x1, x2=x2, y1=y1, y2=y2)
+        elif event.button == 2:  # Right Mouse Button
+            self._model.on_right_mouse_drag(x1=x1, y1=y1, x2=x2, y2=y2)
+
+    def mouse_wheel(self, event: SceneMouseEvent):
+        self._model.on_mousewheel_move(increment=int(event.delta[1]))
 
     def update(self, changed: str) -> None:
         render_funs = {
