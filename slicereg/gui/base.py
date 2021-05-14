64b9f9bdf3be75802a8ffff103773016b1feb4dd
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-14 22:31:32 +0200

    removed unused BaseView

--- slicereg/gui/base.py
+++ slicereg/gui/base.py
@@ -8,18 +8,3 @@ class BaseQtWidget(ABC):
     @property
     @abstractmethod
     def qt_widget(self) -> QWidget: ...
-
-
-class BaseView(ABC):
-
-    @abstractmethod
-    def update(self, **kwargs) -> None:
-        ...
-
-    def on_registration(self, model):
-        """Overwriteable method that's called after the viewmodel is registered."""
-        pass
-
-    def register(self, model) -> None:
-        model.updated.connect(self.update)
-        self.on_registration(model=model)
