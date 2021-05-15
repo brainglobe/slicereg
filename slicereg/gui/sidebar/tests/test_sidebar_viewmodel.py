42517802c55fdf9290ddfa9369d938d0a5690890
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-15 17:58:22 +0200

    property testing resolution property

--- slicereg/gui/sidebar/tests/test_sidebar_viewmodel.py
+++ slicereg/gui/sidebar/tests/test_sidebar_viewmodel.py
@@ -1,21 +1,37 @@
+from functools import partial
 from unittest.mock import Mock
 
-import numpy as np
 import pytest
+from hypothesis import given
+from hypothesis.strategies import floats
+from pytest import approx
 
 from slicereg.app.app_model import AppModel
 from slicereg.gui.sidebar.viewmodel import SidebarViewModel
 from slicereg.utils import DependencyInjector
 
+sensible_floats = partial(floats, allow_nan=False, allow_infinity=False)
 
-@pytest.fixture
-def view_model():
-    image = np.linspace(0, 10, num=10)[:, np.newaxis]
-    model = AppModel(_injector=Mock(DependencyInjector), section_image=image)
-    view = SidebarViewModel(_model=model)
-    return view
+@pytest.fixture(scope='module')
+def app_model():
+    app_model = AppModel(_injector=Mock(DependencyInjector))
+    return app_model
 
 
-def test_resolution_updated_with_section_text_change(view_model: SidebarViewModel):
-    view_model.section_resolution_text = '6.28'
-    assert view_model._model.section_image_resolution == 6.28
+@pytest.fixture(scope='module')
+def view_model(app_model):
+    return SidebarViewModel(_model=app_model)
+
+
+@given(resolution=sensible_floats(0.01, 100))
+def test_resolution_updated_with_section_text_change(resolution, view_model: SidebarViewModel):
+    view_model.section_resolution_text = str(resolution)
+    assert view_model._model.section_image_resolution == approx(resolution)
+
+
+@given(resolution=sensible_floats(0.01, 100))
+def test_viewmodel_resolution_updated_when_app_updates(resolution, view_model: SidebarViewModel, app_model):
+    app_model.section_image_resolution = resolution
+    assert view_model.section_resolution_text == str(resolution)
+
+
