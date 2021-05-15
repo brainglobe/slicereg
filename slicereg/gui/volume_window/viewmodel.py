ef27893e7d03603a9627b5aca0174d34cf4c71ea
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-15 13:10:32 +0200

    refactor: elevate conditional to dict of methods

--- slicereg/gui/volume_window/viewmodel.py
+++ slicereg/gui/volume_window/viewmodel.py
@@ -20,26 +20,44 @@ class VolumeViewModel(HasObservableAttributes):
         self._model.register(self.update)
 
     def update(self, changed: str):
-        if changed == 'section_image' and (image := self._model.section_image) is not None:
+        update_funs = {
+            'section_image': self._update_section_image,
+            'section_transform': self._update_section_transform,
+            'clim_3d_values': self._update_clim,
+            'visible_volume': self._switch_visible_volume,
+            'registration_volume': self._update_visible_volume_to_registration,
+            'annotation_volume': self._update_visible_volume_to_annotation,
+        }
+        if (fun := update_funs.get(changed)) is not None:
+            fun()
+
+    def _update_section_image(self):
+        if (image := self._model.section_image) is not None:
             self.section_image = image
-        elif changed == 'section_transform' and (transform := self._model.section_transform) is not None:
+
+    def _update_section_transform(self):
+        if (transform := self._model.section_transform) is not None:
             self.section_transform = transform
-        elif changed == 'clim_3d_values':
-            self.clim = self._model.clim_3d_values
-        elif changed == 'visible_volume':
-            m = self._model
-            if m.visible_volume == VolumeType.REGISTRATION and (volume := m.registration_volume) is not None:
-                self.atlas_volume = volume
-            elif m.visible_volume == VolumeType.ANNOTATION and m.annotation_volume is not None:
-                self.atlas_volume = m.annotation_volume
-        elif changed == 'registration_volume':
-            m = self._model
-            if m.visible_volume == VolumeType.REGISTRATION and m.registration_volume is not None:
-                self.atlas_volume = m.registration_volume
-        elif changed == 'annotation_volume':
-            m = self._model
-            if m.visible_volume == VolumeType.ANNOTATION and m.annotation_volume is not None:
-                self.atlas_volume = m.annotation_volume
+
+    def _update_clim(self):
+        self.clim = self._model.clim_3d_values
+
+    def _switch_visible_volume(self):
+        m = self._model
+        if m.visible_volume == VolumeType.REGISTRATION and (volume := m.registration_volume) is not None:
+            self.atlas_volume = volume
+        elif m.visible_volume == VolumeType.ANNOTATION and m.annotation_volume is not None:
+            self.atlas_volume = m.annotation_volume
+
+    def _update_visible_volume_to_registration(self):
+        m = self._model
+        if m.visible_volume == VolumeType.REGISTRATION and m.registration_volume is not None:
+            self.atlas_volume = m.registration_volume
+
+    def _update_visible_volume_to_annotation(self):
+        m = self._model
+        if m.visible_volume == VolumeType.ANNOTATION and m.annotation_volume is not None:
+            self.atlas_volume = m.annotation_volume
 
     @property
     def camera_center(self) -> Tuple[float, float, float]:
