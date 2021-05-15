ba3bff41a8d7b0687b6b0fdb61650932d59c199d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-16 01:27:30 +0200

    test iomio reader

---
+++ slicereg/io/imio/tests/test_imio.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+import numpy as np
+
+from slicereg.core.atlas import Atlas
+from slicereg.io import ImioLocalAtlasReader
+
+
+def test_imio_reader_creates_atlas():
+    reader = ImioLocalAtlasReader()
+    with patch("slicereg.io.imio.atlas.imio.load_any") as load_any:
+        load_any.return_value = np.empty((5, 5, 5), dtype=np.uint16)
+        atlas = reader.read("super_atlas", resolution_um=10)
+
+    assert isinstance(atlas, Atlas)
+    assert atlas.resolution_um == 10
