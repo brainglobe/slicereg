e7e4008575077a3b69252b7e1884fdd6e58d043d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-15 20:07:27 +0200

    test viewmodel-to-view update calls

--- slicereg/gui/sidebar/tests/test_sidebar_view.py
+++ slicereg/gui/sidebar/tests/test_sidebar_view.py
@@ -4,6 +4,7 @@ import pytest
 
 from slicereg.app.app_model import AppModel
 from slicereg.gui.sidebar import SidebarView, SidebarViewModel
+from slicereg.utils.introspection import get_public_attrs
 
 
 @pytest.fixture
@@ -18,6 +19,14 @@ def view(view_model):
 
 def test_sidebar_view_launches_without_errors(qtbot, view):
     qtbot.addWidget(view.qt_widget)
+
+
+def test_slice_view_updates_without_error_for_all_viewmodel_fields(qtbot):
+    for attr in get_public_attrs(SidebarViewModel):
+        model = SidebarViewModel(_model=Mock(AppModel))
+        view = SidebarView(_model=model)
+        qtbot.addWidget(view.qt_widget)
+        setattr(model, attr, getattr(model, attr))  # set attribute with its own value
 
 
 def test_sidebar_show_image_dialog_doesnt_crash(qtbot, view):
