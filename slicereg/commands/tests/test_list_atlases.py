b1fe59e6265049078e5a24cee6527d0b39114d5e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-05-16 16:16:18 +0200

    list atlases test on mocked reader, not patched brainglobe

--- slicereg/commands/tests/test_list_atlases.py
+++ slicereg/commands/tests/test_list_atlases.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
-from unittest.mock import patch
+from unittest.mock import Mock
 
+from slicereg.commands.base import BaseRemoteAtlasReader
 from slicereg.commands.list_atlases import ListRemoteAtlasesCommand
-from slicereg.io import BrainglobeRemoteAtlasReader
 
 
 def test_list_atlases_gets_brainglobe_atlas_names():
 
-    load_atlases = ListRemoteAtlasesCommand(_remote_atlas_reader=BrainglobeRemoteAtlasReader())
+    atlases = ['allen_rhinoceros_1000um', 'mpi_zebrafinch_1um']
 
-    fake_atlases = [('allen_rhinoceros_1000um', '1.0'), ('mpi_zebrafinch_1um', '1.2')]
-    with patch("slicereg.io.brainglobe.atlas.bg_utils.conf_from_url", return_value={'atlases': fake_atlases}):
-        result = load_atlases()
+    reader = Mock(BaseRemoteAtlasReader)
+    reader.list.return_value = atlases
 
-    assert result.atlas_names == ['allen_rhinoceros_1000um', 'mpi_zebrafinch_1um']
+    list_atlases = ListRemoteAtlasesCommand(_remote_atlas_reader=reader)
+
+    result = list_atlases()
+    assert result.atlas_names == atlases
