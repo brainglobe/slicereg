69cbed6dbccadf75fa9f5d0b79d983d41ed86aed
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-06-07 16:33:56 +0200

    mocked out Section and Atlas in InMemoryRepo tests

--- slicereg/repos/tests/test_repo.py
+++ slicereg/repos/tests/test_repo.py
@@ -1,10 +1,7 @@
 from unittest.mock import Mock
 from uuid import uuid4
 
-import numpy as np
-
 from slicereg.core.atlas import Atlas
-from slicereg.core.image import Image
 from slicereg.core.section import Section
 from slicereg.repos import InMemoryRepo
 
@@ -16,15 +13,15 @@ def test_repo_starts_with_no_atlas():
 
 def test_repo_can_round_trip_atlas():
     repo = InMemoryRepo()
-    atlas = Atlas(volume=np.random.random((3, 3, 3)), resolution_um=1)
+    atlas = Mock(Atlas)
     repo.set_atlas(atlas=atlas)
     assert repo.get_atlas() == atlas
 
 
 def test_repo_stores_multiple_sections():
     repo = InMemoryRepo()
-    section1 = Section.create(image=Image(channels=np.empty((2, 3, 4)), resolution_um=12))
-    section2 = Section.create(image=Image(channels=np.empty((2, 3, 4)), resolution_um=12))
+    section1 = Mock(Section, id='a')
+    section2 = Mock(Section, id='b')
     assert len(repo.get_sections()) == 0
     repo.save_section(section=section1)
     assert len(repo.get_sections()) == 1
@@ -34,13 +31,13 @@ def test_repo_stores_multiple_sections():
 
 def test_repo_overwrites_existing_section_even_if_properties_change():
     repo = InMemoryRepo()
-    section = Section.create(image=Image(channels=np.empty((2, 3, 4)), resolution_um=12))
+    section = Mock(Section, id=3)
     repo.save_section(section=section)
     assert len(repo.get_sections()) == 1
     repo.save_section(section=section)
     assert len(repo.get_sections()) == 1
 
-    section_moved = section.update(image=section.image.update(resolution_um=14))
+    section_moved = Mock(Section, id=section.id)
     repo.save_section(section=section_moved)
     assert len(repo.get_sections()) == 1
 
